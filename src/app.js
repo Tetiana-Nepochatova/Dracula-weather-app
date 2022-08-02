1cae39121cfe9b7320b30502b0f6c0e6d3d1df1d
Tetiana Nepochatova <tyanuan@gmail.com>
2022-08-02 14:39:24 +0200

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,44 @@
+function formatDate(timestamp) {
+  let dateToday = new Date(timestamp);
+  let hours = dateToday.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = dateToday.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let date = dateToday.getDate();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dateToday.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[dateToday.getMonth()];
+  let year = dateToday.getFullYear();
+
+  return `${day} ${hours}:${minutes} </br> ${date} ${month} ${year}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
 
@@ -6,11 +47,13 @@ function displayTemperature(response) {
   let feelElement = document.querySelector(`#feel`);
   let humidityElement = document.querySelector(`#humidity`);
   let windElement = document.querySelector(`#wind`);
+  let dateElement = document.querySelector(`#date`);
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   feelElement.innerHTML = Math.round(response.data.main.feels_like);
   humidityElement.innerHTML = Math.round(response.data.main.humidity);
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = `7606e398d85035b10d42b33b84e4a65a`;
